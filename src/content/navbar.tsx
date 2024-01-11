67a78068a3fff6f0b2ac84b2c8afe10d3ae94f80
Tahsin Mazumdar <tahsin.mazumdar@gmail.com>
2024-01-11 18:27:44 -0500

    added working search text and button input

--- src/content/navbar.tsx
+++ src/content/navbar.tsx
@@ -11,6 +11,19 @@ export function NavBar({searchText, setSearchText, onSearchClick }: navBarProps)
         setSearchText(e.target.value);
     }
 
+import { ChangeEvent } from "react"
+
+type navBarProps = {
+    searchText: string,
+    setSearchText: React.Dispatch<React.SetStateAction<any>>,
+    onSearchClick: () => void
+}
+
+export function NavBar({searchText, setSearchText, onSearchClick }: navBarProps) {
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearchText(e.target.value);
+    }
+
     return (
         <div className="navbar bg-base-200 rounded-sm">
             <div className="flex-2 px-2">
@@ -20,6 +33,7 @@ export function NavBar({searchText, setSearchText, onSearchClick }: navBarProps)
                     {/* sun icon */}
                     <svg className="swap-on fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/></svg>
                     {/* moon icon */}
+                    <svg className="swap-off fill-stone-400 w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"/></svg>
                     <svg className="swap-off fill-stone-400 w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"/></svg>
                 </label>
             </div>
@@ -32,9 +46,18 @@ export function NavBar({searchText, setSearchText, onSearchClick }: navBarProps)
                 </div>
             </div>
             <div className="px-1">
+            <div className="flex-1">
+                <h1>Ticker Tracker 💸 </h1>
+            </div>
+            <div className="px-1">
+                <div className="form-control">
+                    <input type="text" placeholder="Ticker" onChange={handleChange} value={searchText} className="input input-primary input-bordered w-24 md:w-auto" />
+                </div>
+            </div>
+            <div className="px-1">
                 <div className="form-control">
                     <button className="btn " onClick={onSearchClick}>
-                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                        <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" className="w-6 h-6"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                         Search
                     </button>
                 </div>
