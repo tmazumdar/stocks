7d2cb9d9d3376bf385420ad22a93dbdbb48a925c
Tahsin Mazumdar <tahsin.mazumdar@gmail.com>
2024-01-11 18:27:44 -0500

    added working search text and button input

--- src/content/content.tsx
+++ src/content/content.tsx
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import { NavBar } from './navbar';
 import { ResultsTable } from './results-table';
+import { net } from 'electron';
 
 export function Content() {
+    
+    const [searchText, setSearchText] = useState("");
+    const searchClickHandler = () => {
+        console.log(`search clicked: ${searchText}`);
+    }
+
     return (
         <>
-            <NavBar></NavBar>
+            <NavBar searchText={searchText} setSearchText={setSearchText} onSearchClick={searchClickHandler}></NavBar>
             <div className="divider divider-primary"></div>
             <ResultsTable></ResultsTable>
         </>
